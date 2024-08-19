e643a4dd047f806b1df683ebde3f0fdda05caa60
Jefferson Vieira da Silva <jefferson98.vieira@gmail.com>
2024-08-19 15:06:25 -0300

    feat: add toast component
    also configure it on app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Toaster } from '@/components/ui/toaster';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -23,6 +24,8 @@ export default function RootLayout({
         <AuthProvider>{children}</AuthProvider>
 
         <Footer />
+
+        <Toaster />
       </body>
     </html>
   );
